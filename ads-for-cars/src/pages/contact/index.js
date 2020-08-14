65cad0eb108b4b9d6eed968f3f919f92cb330b73
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2020-08-14 23:17:46 +0300

    fix: add google maps functionality

--- ads-for-cars/src/pages/contact/index.js
+++ ads-for-cars/src/pages/contact/index.js
@@ -2,19 +2,36 @@ import React from "react";
 import PageLayout from "../../components/page-layout";
 import Title from "../../components/title";
 import styles from "./index.module.css";
+import GoogleMapReact from 'google-map-react';
 
 const ContactsPage = () => {
+
+    const renderMarkers = (map, maps) => {
+        let marker = new maps.Marker({
+            position: { lat: 42.131155, lng: 24.793281 },
+            map,
+            title: `Car-Magazine`
+        });
+        return marker;
+    };
 
     return (
         <PageLayout>
             <Title title="Contacts" />
             <div className={styles.container}>
-                <p>
+                <div>
                     Some mobile phones, address and Google-Maps maybe...:)
-                </p>
-                <p>
-                    
-                </p>
+                </div>
+                <div className={styles.map}>
+                    <GoogleMapReact
+                        bootstrapURLKeys={{ key: 'AIzaSyCe0KoGzgsMjtwjyQi6wWopCX62qimjAIM' }}
+                        defaultCenter={{ lat: 42.131155, lng: 24.793281 }}
+                        defaultZoom={17}
+                        yesIWantToUseGoogleMapApiInternals
+                        onGoogleApiLoaded={({ map, maps }) => renderMarkers(map, maps)}
+                    >
+                    </GoogleMapReact>
+                </div>
             </div>
         </PageLayout>
     )
