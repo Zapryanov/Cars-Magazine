d550c9bb39b662639e877716e8a5c791497d5396
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2020-08-15 15:47:42 +0300

    fix: Profile page is migrated to the function component with React Hook

--- ads-for-cars/src/pages/profile/index.js
+++ ads-for-cars/src/pages/profile/index.js
@@ -1,67 +1,62 @@
-import React, { Component } from "react"
+import React, { useState, useEffect } from "react"
 import PageLayout from "../../components/page-layout";
 import styles from "./index.module.css";
-import UserContext from "../../Context";
 import Title from "../../components/title";
 import Car from "../../components/car";
-import { withRouter } from "react-router-dom";
+import { withRouter, useRouteMatch, useHistory } from "react-router-dom";
 
-class ProfilePage extends Component {
-    constructor(props) {
-        super(props);
+const ProfilePage = (props) => {
 
-        this.state = {
-            username: null,
-            postsCount: 0,
-            posts: null
-        }
-    }
+    const [state, setState] = useState({
+        username: null,
+        postsCount: 0,
+        posts: null
+    })
 
-    static contextType = UserContext
+    const match = useRouteMatch();
+    const history = useHistory();
 
-    componentDidMount() {
-        this.getUser(this.props.match.params.userId);
-    }
-
-    getUser = async (id) => {
+    const getUser = async (id) => {
         const response = await fetch(`http://localhost:9999/api/user?id=${id}`);
 
         if (!response.ok) {
-            this.props.history.push("/error")
+            history.push("/error")
         }
         const user = await response.json();
 
-        this.setState({
+        setState({
             username: user.username,
             postsCount: user.posts && user.posts.length,
             posts: user.posts
         })
     }
 
+    useEffect(() => {
+        getUser(match.params.userId);
+    })
 
-    render() {
-        const { username, postsCount, posts } = this.state;
-        const cars = posts && posts.map(car => <Car key={car._id} {...car} />);
+    const { username, postsCount, posts } = state;
+    const cars = posts && posts.map(car => <Car key={car._id} {...car} />);
 
-        return (
-            <PageLayout>
-                <Title title="Profile Page" />
-                <div className={styles.container}>
-                    <div>
-                        <p>
-                            <span className={styles.boldText}>User: </span>
-                            <span className={styles.shadowTitle}>{username}</span>
-                        </p>
-                        <p>
-                            <span className={styles.boldText}>Posts: </span>
-                            <span className={styles.price}>{postsCount}</span>
-                        </p>
-                    </div>
-                    {cars}
+    return (
+        <PageLayout>
+            <Title title="Profile Page" />
+            <div className={styles.container}>
+                <div>
+                    <p>
+                        <span className={styles.boldText}>User: </span>
+                        <span className={styles.shadowTitle}>{username}</span>
+                    </p>
+                    <p>
+                        <span className={styles.boldText}>Posts: </span>
+                        <span className={styles.price}>{postsCount}</span>
+                    </p>
                 </div>
-            </PageLayout>
-        )
-    }
+                {cars}
+            </div>
+        </PageLayout>
+    )
+
 }
 
 export default withRouter(ProfilePage)
